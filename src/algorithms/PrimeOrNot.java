2c1ad58c15f45e7199a4061175ecdc51fc72cfe9
ilabstaff <ilabstaff@172.16.116.77>
2019-05-01 19:27:35 -0700

    prime

---
+++ src/algorithms/PrimeOrNot.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+public class PrimeOrNot {
+	public static void main(String[]args) {
+		boolean prime = true;
+		int num = 21;
+		for (int i = 2; i < num; i++) {
+			
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
