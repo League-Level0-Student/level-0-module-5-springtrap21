1a6f86f3c95282a6526cc67636f8536627c1755a
ilabstaff <ilabstaff@172.16.116.77>
2019-06-05 19:26:32 -0700

    goofy__ring__gauntlet

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -17,22 +17,26 @@ public class GoofyNames {
 		System.out.println(hbo.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-		for (int i = 4; i < 6; i++) {
+		for (int i = 0; i < hbo.length(); i++) {
 			
-		}
+		
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+			String Z = hbo.substring(i, i + 1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
+			if (i % 2 == 0) {
+				Z = Z.toUpperCase();
+			} else {
+				Z = Z.toLowerCase();
+			}
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
+			goofyName = goofyName + Z;
+		}
 		// 7. Use pop-up to show user their Goofy name
-
+		JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
