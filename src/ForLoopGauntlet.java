b856d26cb7636bdf7f9601e12aef4d716611ecd2
ilabstaff <ilabstaff@172.16.116.73>
2019-06-12 19:20:27 -0700

    loopThanos

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -21,6 +21,48 @@ public class ForLoopGauntlet {
 				System.out.println(i +" is odd");
 			}
 		}
+		for (int i = 0; i < 784; i += 7) {
+			System.out.println(i);
+		}
+		for (int i = 2008; i < 2019; i += 1) {
+			System.out.println("In " + i + " I was " + (i - 2008)  );
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println(i + " " + j);
+			}
+		}
+		for (int i = 1; i < 8; i += 3) {
+			if (i > 1)
+				System.out.println("");
+			
+			for (int j = 0; j < 3; j += 1) {
+					System.out.print(i + j + " ");
+				
+			}
+		}
+		for (int i = 1; i < 92; i += 10) {
+			System.out.println("");
+			for (int j = 0; j < 10; j +=1) {
+				if (i+j < 10)
+					System.out.print(" " +(i + j) + " ");
+				else
+					System.out.print((i + j) + " ");
+			}
+		}
+		for (int i = 1; i < 7; i++) {
+			System.out.println("");
+			for (int j = 0; j < i; j++) {
+				System.out.print("* ");
+			}
+			if (i == 6)
+				System.out.println("");
+		}
+		for (int i = 0; i < 101; i++) {
+			System.out.println(100 - i);
+		}
 		
 	}
 }
+
+
