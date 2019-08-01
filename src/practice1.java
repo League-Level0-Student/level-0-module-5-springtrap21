de0ee2af69f81e1e4c01d1df72b5d007566273f7
ilabstaff <ilabstaff@172.16.116.79>
2019-07-31 19:30:22 -0700

    summary1

---
+++ src/practice1.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class practice1 {
+public static void main(String[] args) {
+	//JOptionPane.showMessageDialog(null, "new big Macs 2 for 2");
+	//String food =JOptionPane.showInputDialog("what is your favorite food");
+	//JOptionPane.showMessageDialog(null, "I always knew you liked " + food);
+	//String brand = JOptionPane.showInputDialog("what is your favorite car brand?");
+	//String car = JOptionPane.showInputDialog("what is your favorite car from the brand you picked?");
+	//JOptionPane.showMessageDialog(null, "you like the " + brand + " " + car);
+	//String veneno = JOptionPane.showInputDialog("how much money u got?");
+	//double money = Double.parseDouble(veneno);
+	//if (money >= 2.50 ) {
+		//System.out.println("you can afford tacos");
+	//}
+		//else {
+			//System.out.println("you can't afford tacos");
+	//for (int i = 100; i < 1001; i++) {
+		//System.out.println(i);
+	//}
+	//Random rnd = new Random();
+	//int jeff = rnd.nextInt(100);
+	//jackIntheBox(0);
+		}
+//public static void jackIntheBox(int sup) {
+	//sup++;
+	//System.out.println(sup);
+//}
+
+	}
+
